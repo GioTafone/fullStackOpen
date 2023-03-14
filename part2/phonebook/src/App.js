2028bf59e0f1266e8075b751af507bb2992120f5
Giovanni Tafone <giola@Giovannis-MacBook-Air.local>
2023-03-14 15:50:18 +0200

    ex 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handlePersonChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const newPersonsObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(newPersonsObject));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
