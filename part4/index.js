d090331acb77b2cc069936e1cc09a2180a237061
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-04-28 16:11:12 +0300

    logger add

--- part4/index.js
+++ part4/index.js
@@ -5,22 +5,7 @@ require("dotenv").config();
 const morgan = require("morgan");
 
 const blogsRouter = require('./controllers/blogs')
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+const middleware = require('./utils/middleware')
 
 app.use(cors());
 app.use(express.json());
@@ -38,8 +23,8 @@ app.use(
 
 app.use('/api/blogs', blogsRouter)
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
